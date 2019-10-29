c7de0232f2cbb9b518534be0339902a539dbe2e7
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 17:21:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,11 @@
 class Application
 
-  def call
+  def call(env)
+    return [200, {'Content-Type' => 'text/html'}, time_message]
+  end
+
+  def time_message
+    (Time.now.to_i < 12) ? ['<em> Good morning! </em>'] : ['<em> Good afternoon! </em>']
   end
 
 end
